4eb2f03e2171b6470773c103c0aedce52bb471af
peterhpchen <peterhsinpingchen@gmail.com>
2021-08-19 23:52:54 +0800

    combo all you can lint first version

---
+++ days/16-combo-all-you-can-lint/examples/finish-project/.markdownlint.js
@@ -0,0 +1,7 @@
+module.exports = {
+  default: true,
+  // Prettier overrides
+  'line-length':  false, // `printWidth`
+  'no-multiple-blanks': false, // Rationale: Empty lines
+  'list-marker-space': false, // Rationale
+};
