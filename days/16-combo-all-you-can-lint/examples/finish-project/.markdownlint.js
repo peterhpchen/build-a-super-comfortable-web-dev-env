6c2488db3621b2a3420f7175018b0f1503e0b097
peterhpchen <peterhsinpingchen@gmail.com>
2021-10-08 23:00:17 +0800

    chore: remove redundant days

--- days/16-combo-all-you-can-lint/examples/finish-project/.markdownlint.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  default: true,
-  // Prettier overrides
-  'line-length':  false, // `printWidth`
-  'no-multiple-blanks': false, // Rationale: Empty lines
-  'list-marker-space': false, // Rationale
-};
